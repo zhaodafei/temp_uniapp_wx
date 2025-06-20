5c94eb299f09bbd4fa72f84d583af254e6221e24
zhaodafei111 <1097625354@qq.com>
2025-06-20 20:20:12 +0800

    1

---
+++ pages/info/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">{{ title }}ssssssssssss </text>
+    </view>
+
+    <view class="text-area">
+      <text class="title" style="color: #ff6b81">{{ content }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { onLoad, onReady } from "@dcloudio/uni-app";
+
+const title = ref("信息查看");
+const content = ref("信息查看");
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
---
+++ pages/my/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+
+    <view class="text-area">
+      <text class="title" style="color: #ff6b81">{{ content }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { onLoad, onReady } from "@dcloudio/uni-app";
+
+const title = ref("我的模块");
+const content = ref("我的数据内容");
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
